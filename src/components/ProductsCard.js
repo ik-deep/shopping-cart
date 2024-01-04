bad17da1f3588532f175c24d47c469e5714cdb8f
irfankhan <er.irfan2798@gmail.com>
2024-01-04 17:50:14 +0530

    first-commit

---
+++ src/components/ProductsCard.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../store/slices/cartSlice';
+
+
+const ProductsCard = (props) => {
+
+    const { images, rating, title, price } = props;
+
+    const [isAdded, setIsAdded] = useState(false);
+
+
+    const dispatch = useDispatch();
+
+
+    const handleAddToCart = () => {
+
+        // here, we cannot directly pass the `props` as it is, if we need to access the same value within the child component. So, we've to pass it as a different prop like this- `{...props}`
+        const item = { ...props };
+        dispatch(addItem(item));
+
+        setIsAdded(true);
+
+        setTimeout(() => {
+            setIsAdded(false);
+        }, 3000);
+    };
+
+
+    return (
+        <>
+            <div className="product_card">
+                <figure>
+                    <img src={images[0]} alt="item-img" />
+                </figure>
+                {/* <strong className="rating">{rating}</strong> */}
+                <h4 className="title">{title}</h4>
+                <h3 className="price">₹ {price.toLocaleString()}</h3>
+                <footer> <button
+                    type="button"
+                    className={`btn ${isAdded ? 'added' : ''}`}
+                    onClick={handleAddToCart}
+                >
+                    {isAdded ? 'Added' : 'Add to cart'}
+                </button></footer>
+               
+            </div>
+        </>
+    );
+};
+
+export default ProductsCard;
